c82c2d892c1989514f0b03851e3ad4f5082939ff
Siddharth1174 <142529706+Siddharth1174@users.noreply.github.com>
2024-05-07 10:08:16 +0530

    Create Array_to_linked_list.java

---
+++ Linked List/Array_to_linked_list.java
@@ -0,0 +1,12 @@
+public static Node constructLL(int []arr) {
+        // Write your code here
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1; i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+
+        }
+        return head;
+    }
