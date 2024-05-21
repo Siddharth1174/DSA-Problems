bf8dc03e52580b7cea68f183f6ce593c27ebb545
Siddharth1174 <142529706+Siddharth1174@users.noreply.github.com>
2024-05-21 09:08:10 +0530

    Create N meetings in one room.java

---
+++ Greedy Algorithm/N meetings in one room.java
@@ -0,0 +1,57 @@
+//Problem Link:
+https://www.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+//Solution:
+class meeting{
+    int start;
+    int end;
+    int pos;
+    
+    meeting(int start, int end, int pos)
+    {
+        this.start = start;
+        this.end = end;
+        this.pos = pos;
+    }
+}
+class meetingComparator implements Comparator<meeting>
+{
+    @Override
+    public int compare(meeting o1, meeting o2)
+    {
+        if(o1.end<o2.end) return -1;
+        else if(o1.end>o2.end) return 1;
+        else if(o1.pos<o2.pos) return -1;
+        return 1;
+    }
+}
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        // add your code here
+        ArrayList<meeting> meet = new ArrayList<>();
+        
+        for(int i= 0; i<start.length; i++){
+            meet.add(new meeting(start[i], end[i], i+1));
+        }
+        meetingComparator mc = new meetingComparator();
+        Collections.sort(meet,mc);
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(meet.get(0).pos);
+        int limit = meet.get(0).end; 
+        
+        for(int i = 1;i<start.length;i++) {
+            if(meet.get(i).start > limit) {
+                limit = meet.get(i).end; 
+                answer.add(meet.get(i).pos);
+            }
+        }
+        //System.out.println("The order in which the meetings will be performed is ");
+        // for(int i = 0;i<answer.size(); i++) {
+        //     System.out.print(answer.get(i) + " ");
+        // }
+        return answer.size();
+    }
+}
