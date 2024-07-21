6825ec5b36463f657f6a20f86433d9730460d5da
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-07-21 16:29:30 +0530

    Create 53.Maximum Subarray.java

---
+++ Arrays/53.Maximum Subarray.java
@@ -0,0 +1,20 @@
+//Problem Link:
+https://leetcode.com/problems/maximum-subarray/description/
+//Solution:
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i<n ;i++){
+            sum+=nums[i];
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+}
