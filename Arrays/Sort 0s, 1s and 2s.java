096563ba7b9e74cdcf268463d623dd1ea9870175
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-08-16 23:36:22 +0530

    Create Sort 0s, 1s and 2s.java

---
+++ Arrays/Sort 0s, 1s and 2s.java
@@ -0,0 +1,11 @@
+//Problem Link:
+https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1?page=1&sortBy=submissions
+//Solution:
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        Arrays.sort(a);
+    }
+}
