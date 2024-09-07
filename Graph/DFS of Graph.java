e8ee6361307ad859d5ccc06b3fb4adcf2944d68f
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-09-07 18:13:42 +0530

    Create DFS of Graph.java

---
+++ Graph/DFS of Graph.java
@@ -0,0 +1,24 @@
+//Problem Link:
+https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+//Solution:
+
+class Solution {
+    public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls){
+        vis[node] = true;
+        ls.add(node);
+        for(Integer it: adj.get(node)){
+            if(vis[it]==false){
+                dfs(it, vis, adj, ls);
+            }
+        }
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean vis[] = new boolean[V+1];
+        vis[0] = true;
+        ArrayList<Integer> ls = new ArrayList<>();
+        dfs(0, vis, adj, ls);
+        return ls;
+    }
+}
