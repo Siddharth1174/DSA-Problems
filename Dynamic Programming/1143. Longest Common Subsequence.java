ea4b26acbccd6767b56d855bbd187b08c3a6e308
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-11-11 18:21:29 +0530

    Create 1143. Longest Common Subsequence.java

---
+++ Dynamic Programming/1143. Longest Common Subsequence.java
@@ -0,0 +1,25 @@
+Problem Link:https://leetcode.com/problems/longest-common-subsequence/description/
+Solution:
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int prev[] = new int[m+1];
+        int cur[] = new int[m+1];
+
+        for(int ind1 = 1;ind1<=n; ind1++){
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1))
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                else
+                    cur[ind2] = Math.max(prev[ind2], cur[ind2 - 1]);
+            }
+            prev = (int[]) (cur.clone());
+        }
+        return prev[m];
+        
+
+    }
+    
+}
