df6e7c5a7a5eb59e4525110d826d44017dc80703
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2025-09-25 16:11:06 +0530

    Create Union_of_two_arrays.java

---
+++ Arrays/Union_of_two_arrays.java
@@ -0,0 +1,28 @@
+//Question: https://www.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+
+        //Adding nums1 elements to set1 :
+        for(int num: nums1){
+            set1.add(num);
+        }
+
+        //Checking for common ones
+        for(int num: nums2){
+            if(set1.contains(num)){
+                result.add(num);
+            }
+        }
+
+        //converting result set to an array
+        int ans[] = new int[result.size()];
+        int i = 0;
+        for(int num: result){
+            ans[i++]=num;
+        }
+
+        return ans;
+    }
+}
